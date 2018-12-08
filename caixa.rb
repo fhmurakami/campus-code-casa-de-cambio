31d27d83dc70272e8eeffc98fdc27a772f3b872f
Felipe H Murakami <fhmurakami@gmail.com>
2018-12-08 10:29:24 -0200

    Add cashiers table to cambio.db and create a method to verify if cashier exists (caixa_existe?)

--- caixa.rb
+++ caixa.rb
@@ -2,24 +2,71 @@ require 'sqlite3'
 require_relative 'operacao'
 
 class Caixa
-attr_accessor :cotacao, :total_dolar, :total_real, :operacoes
+  attr_accessor :data, :cotacao, :total_dolar, :total_real, :operacoes
 
-@@db = SQLite3::Database.open "cambio.db"
+  @@db = SQLite3::Database.open 'cambio.db'
+
+  # Cria a tabela de caixas
+  @@db.execute <<-SQL
+    CREATE TABLE IF NOT EXISTS cashiers (
+      id integer primary key autoincrement not null,
+      data_caixa text not null,
+      cotacao real not null,
+      qtd_dolar real not null,
+      qtd_real real not null
+    );
+  SQL
+
+  @@db.close
 
   def initialize
     @operacoes = []
+    @data = DateTime.now.strftime('%d/%m/%Y')
 
-    print 'Cotação do dólar em reais: '
-    @cotacao = gets.to_f
-    print 'Dolares em caixa: '
-    @total_dolar = gets.to_f
-    print 'Reais em caixa: '
-    @total_real = gets.to_f
+    if caixa_existe?
+      @@db = SQLite3::Database.open 'cambio.db'
+      @@db.results_as_hash = true
+      @@db.execute('SELECT * FROM cashiers WHERE data_caixa = ?', [@data]) do |caixa|
+        @cotacao = caixa['cotacao']
+        @total_dolar = caixa['qtd_dolar']
+        @total_real = caixa['qtd_real']
+      end
+      puts "Cotação: #{cotacao}"
+      puts "Dólares em caixa: #{total_dolar}"
+      puts "Reais em caixa: #{total_real}"
+      puts
+    else
+      print 'Cotação do dólar em reais: '
+      @cotacao = gets.to_f
+      print 'Dolares em caixa: '
+      @total_dolar = gets.to_f
+      print 'Reais em caixa: '
+      @total_real = gets.to_f
+
+      @@db = SQLite3::Database.open 'cambio.db'
+      @@db.execute('INSERT INTO cashiers (data_caixa, cotacao, qtd_dolar, qtd_real) VALUES (?, ?, ?, ?)', [@data, @cotacao, @total_dolar, @total_real])
+    end
+    @@db.close
+  end
+
+  def caixa_existe?
+    # data_atual = DateTime.now.strftime('%d/%m/%Y')
+
+    @@db = SQLite3::Database.open 'cambio.db'
+    @@db.results_as_hash = true
+    @@db.execute('SELECT * FROM cashiers') do |caixa|
+      puts caixa[:data_caixa]
+      return true if @data == caixa[:data_caixa]
+
+      false
+    end
+
+    @@db.close
   end
 
   def comprar_dolar(dolar)
     real = (dolar * cotacao)
-    operacao = Operacao.new('compra', 'dolar', cotacao, dolar)
+    operacao = Operacao.new(tipo: 'compra', moeda: 'dolar', cotacao: cotacao, total: dolar)
     operacao.valor_total(dolar, real, 'compra', 'dolar')
     if real > total_real
       puts 'Saldo insuficiente.'
@@ -30,7 +77,7 @@ attr_accessor :cotacao, :total_dolar, :total_real, :operacoes
         self.total_real -= real
         operacoes << operacao
         operacao.salvar
-        puts "Transação efetuada com sucesso"
+        puts 'Transação efetuada com sucesso'
         puts "Qtd dolar: #{total_dolar}, qtd real: #{total_real}"
       end
     end
@@ -38,7 +85,7 @@ attr_accessor :cotacao, :total_dolar, :total_real, :operacoes
 
   def vender_dolar(dolar)
     real = (dolar * cotacao)
-    operacao = Operacao.new('venda', 'dolar', cotacao, dolar)
+    operacao = Operacao.new(tipo: 'venda', moeda: 'dolar', cotacao: cotacao, total: dolar)
     operacao.valor_total(dolar, real, 'venda', 'dolar')
     if dolar > total_dolar
       puts 'Saldo insuficiente.'
@@ -49,7 +96,7 @@ attr_accessor :cotacao, :total_dolar, :total_real, :operacoes
         self.total_real += real
         operacoes << operacao
         operacao.salvar
-        puts "Transação efetuada com sucesso"
+        puts 'Transação efetuada com sucesso'
         puts "Qtd dolar: #{total_dolar}, qtd real: #{total_real}"
       end
     end
@@ -57,7 +104,7 @@ attr_accessor :cotacao, :total_dolar, :total_real, :operacoes
 
   def comprar_reais(real)
     dolar = (real / cotacao)
-    operacao = Operacao.new('compra', 'real', cotacao, dolar)
+    operacao = Operacao.new(tipo: 'compra', moeda: 'real', cotacao: cotacao, total: dolar)
     operacao.valor_total(dolar, real, 'compra', 'real')
     if dolar > total_dolar
       puts 'Saldo insuficiente.'
@@ -68,7 +115,7 @@ attr_accessor :cotacao, :total_dolar, :total_real, :operacoes
         self.total_real += real
         operacoes << operacao
         operacao.salvar
-        puts "Transação efetuada com sucesso"
+        puts 'Transação efetuada com sucesso'
         puts "Qtd dolar: #{total_dolar}, qtd real: #{total_real}"
       end
     end
@@ -76,7 +123,7 @@ attr_accessor :cotacao, :total_dolar, :total_real, :operacoes
 
   def vender_reais(real)
     dolar = (real / cotacao)
-    operacao = Operacao.new('venda', 'real', cotacao, dolar)
+    operacao = Operacao.new(tipo: 'venda', moeda: 'real', cotacao: cotacao, total: dolar)
     operacao.valor_total(dolar, real, 'venda', 'real')
     if real > total_real
       puts 'Saldo insuficiente.'
@@ -87,7 +134,7 @@ attr_accessor :cotacao, :total_dolar, :total_real, :operacoes
         self.total_real -= real
         operacoes << operacao
         operacao.salvar
-        puts "Transação efetuada com sucesso"
+        puts 'Transação efetuada com sucesso'
         puts "Qtd dolar: #{total_dolar}, qtd real: #{total_real}"
       end
     end
@@ -95,17 +142,19 @@ attr_accessor :cotacao, :total_dolar, :total_real, :operacoes
 
   def mostrar_operacoes
     rows = []
-    @@db.execute("SELECT * FROM transactions") do |op|
+    @@db = SQLite3::Database.open 'cambio.db'
+    @@db.execute('SELECT * FROM transactions') do |op|
       rows << op
     end
-    @tabela_op = Terminal::Table.new :title => "Operações", :headings => ['Operação', 'Tipo', 'Moeda', 'Cotação', 'Total (US$)'], :rows => rows
+    @tabela_op = Terminal::Table.new title: 'Operações', headings: ['Operação', 'Tipo', 'Moeda', 'Cotação', 'Total (US$)'], rows: rows
     puts @tabela_op
+    @@db.close
   end
 
   def imprimir
     rows = []
     rows << [cotacao, total_dolar, total_real]
-    table = Terminal::Table.new :title => "Caixa", :headings => ['Cotação', 'Dolares', 'Reais'], :rows => rows
+    table = Terminal::Table.new title: 'Caixa', headings: %w[Cotação Dolares Reais], rows: rows
     puts table
   end
 
--- operacao.rb
+++ operacao.rb
@@ -3,43 +3,36 @@ require 'sqlite3'
 class Operacao
   attr_accessor :id, :tipo, :moeda, :cotacao, :total
 
-if File.file?('cambio.db')
-  @@db = SQLite3::Database.open "cambio.db"
-else
-  #Cria o banco de dados
-  @@db = SQLite3::Database.new "cambio.db"
+  @@db = SQLite3::Database.open 'cambio.db'
 
-  #Cria a tabela
-  @rows = @@db.execute <<-SQL
-  create table transactions (
-    id integer primary key autoincrement not null,
-    tipo text not null,
-    moeda text not null, 
-    cotacao real not null,
-    total real not null
-  );
-SQL
-end
+  # Cria a tabela de transações
+  @@db.execute <<-SQL
+    CREATE TABLE IF NOT EXISTS transactions (
+      id integer primary key autoincrement not null,
+      tipo text not null,
+      moeda text not null,
+      cotacao real not null,
+      total real not null
+    );
+  SQL
 
-  @@ultimo_id = 0
+  @@db.close
 
-  def initialize(tipo, moeda, cotacao, total)
+  def initialize(tipo:, moeda:, cotacao:, total:)
     @tipo = tipo
     @moeda = moeda
     @cotacao = cotacao
     @total = total
-    @id = @@ultimo_id + 1
-    @@ultimo_id = @id
   end
 
   def valor_total(dolar, real, operacao, moeda)
-    if operacao == 'compra' and moeda == 'dolar'
+    if (operacao == 'compra') && (moeda == 'dolar')
       puts "O valor necessário para a operação é R$#{real}."
-    elsif operacao == 'compra' and moeda == 'real'
+    elsif (operacao == 'compra') && (moeda == 'real')
       puts "O valor necessário para a operação é $#{dolar}."
-    elsif operacao == 'venda' and moeda == 'dolar'
+    elsif (operacao == 'venda') && (moeda == 'dolar')
       puts "Essa venda irá render R$#{real}."
-    elsif operacao == 'venda' and moeda == 'real'
+    elsif (operacao == 'venda') && (moeda == 'real')
       puts "Essa venda irá render $#{dolar}."
     else
       puts 'Operação inválida'
@@ -48,13 +41,16 @@ end
 
   def confirmar?
     puts 'Confirmar? [S/N] '
-    resposta = gets.chomp().upcase
+    resposta = gets.chomp.upcase
     return true if resposta == 'S'
+
     puts 'Transação cancelada.'
     false
   end
 
   def salvar
-    @@db.execute("INSERT INTO transactions (tipo, moeda, cotacao, total) VALUES (?, ?, ?, ?)",[self.tipo, self.moeda, self.cotacao, self.total])
+    @@db = SQLite3::Database.open 'cambio.db'
+    @@db.execute('INSERT INTO transactions (tipo, moeda, cotacao, total) VALUES (?, ?, ?, ?)', [tipo, moeda, cotacao, total])
+    @@db.close
   end
 end
